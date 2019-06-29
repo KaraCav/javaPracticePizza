8e3e0af7104f6b63d1797be5fe956526947962c1
Kara Cavanaugh <karacav@msn.com>
2019-06-29 15:03:52 -0400

    Update PizzaOrder.java

--- PizzaOrder.java
+++ PizzaOrder.java
@@ -1,6 +1,10 @@
 public Class PizzaOrder { 
   public static void main(String[] args){
-    
+    Scanner getOrderInfo = new Scanner(System.in);
      System.out.println("Choose a size"); 
+     String userSize = getOrderInfo.nextLine();
+     System.out.println("Would you like a gluten free pizza? Yes/ No"); 
+     String userIsGlutenFree = getOrderInfo.nextLine();
+     System.out.println("You chose a " + userSize + "and said " + userIsGlutenFree + " to needing gluten free.");
     }
   }
